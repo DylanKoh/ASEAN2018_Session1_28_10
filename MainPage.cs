729023e9020e9ae70599687f5554dee3cefa89fa
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-10-28 14:46:20 +0800

    Add project files.

---
+++ MainPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASEAN2018_Session1_28_10
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
